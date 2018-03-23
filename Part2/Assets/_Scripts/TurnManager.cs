bc6e762b1c5e1ce462e2c0330ac289b8b7670119
GideonvDoorn <gideon.vandoorn@student.fontys.nl>
2018-03-23 12:55:45 +0100

    added 4th board

--- Part2/Assets/_Scripts/TurnManager.cs
+++ Part2/Assets/_Scripts/TurnManager.cs
@@ -7,7 +7,7 @@ public class TurnManager : MonoBehaviour {
     public static bool GameProperlyLoaded = false;
 
     public static int currentPlayerIndex = 0;
-    public static int boardIndex =2;
+    public static int boardIndex =3;
     public static int turnAmount = 100;
     public static int currentTurn = 1;
     public static bool TurnInProgress = false;
